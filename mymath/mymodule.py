3bcca17753e91ae47b22e27dc4cd91896beef8e2
atp1998 <156267100+atp1998@users.noreply.github.com>
2026-01-09 14:15:18 +0900

    Add files via upload

---
+++ mymath/mymodule.py
@@ -0,0 +1,9 @@
+def square(number):
+    """This function returns the square of a given number"""
+
+    return number ** 2
+
+def doubler(number):
+    """This function returns twice the value of a given number"""
+
+    return number * 2
